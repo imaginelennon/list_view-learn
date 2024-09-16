29e288fbeb0434be1c3dd9c7a18718e79bece07a
Priyank Sharma <s.priyank92@gmail.com>
2024-09-15 17:04:10 -0700

    Initial Commit

---
+++ list_view learn/list_view_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_view_learnApp.swift
+//  list_view learn
+//
+//  Created by Priyank Sharma on 15/09/24.
+//
+
+import SwiftUI
+
+@main
+struct list_view_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
